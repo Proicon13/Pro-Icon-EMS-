b3620e084797555ea1913516fb8185c2dd7534b3
devmoaid1 <moaidmohamed1312@gmail.com>
2024-12-31 17:20:20 +0200

    removed device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pro_icon/Core/dependencies.dart';
@@ -19,12 +17,9 @@ void main() async {
   setupDependencies();
   await EasyLocalization.ensureInitialized();
   Bloc.observer = AppBlocObserver();
-  runApp(DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => EasyLocalization(
-        path: 'assets/translations',
-        supportedLocales: supportedLocales,
-        fallbackLocale: supportedLocales.first, // en
-        child: const Proicon()),
-  ));
+  runApp(EasyLocalization(
+      path: 'assets/translations',
+      supportedLocales: supportedLocales,
+      fallbackLocale: supportedLocales.first, // en
+      child: const Proicon()));
 }

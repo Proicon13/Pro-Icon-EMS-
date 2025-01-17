2d1afa6e723b8f1856a096e5ea419ba61b7e8c7a
devmoaid1 <moaidmohamed1312@gmail.com>
2025-01-17 21:09:58 +0200

    resolve new comments

--- lib/Features/Mads/Screens/Mads_screen.dart
+++ lib/Features/Mads/Screens/Mads_screen.dart
@@ -1,55 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:pro_icon/Core/constants/app_assets.dart';
-import 'package:pro_icon/Core/theme/app_colors.dart';
-import 'package:pro_icon/Core/theme/app_text_styles.dart';
 import 'package:pro_icon/Core/utils/extensions/size_helper.dart';
-import 'package:pro_icon/Core/utils/responsive_helper/size_config.dart';
+import 'package:pro_icon/Core/utils/extensions/spaces.dart';
+import 'package:pro_icon/Core/utils/responsive_helper/size_constants.dart';
 import 'package:pro_icon/Core/widgets/base_app_Scaffold.dart';
-import 'package:pro_icon/Core/widgets/custom_header.dart';
 import 'package:pro_icon/Features/CategoryDetails/Widget/custom_app_bar.dart';
 import 'package:pro_icon/Features/Mads/Widgets/card_Mads_Widget.dart';
 import 'package:pro_icon/Features/Settings/Screens/settings_view.dart';
-import 'package:pro_icon/Features/home/screens/home_view.dart';
-
 
 class MadsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return BaseAppScaffold(
-
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: SizeConstants.kScaffoldPadding(context),
         child: Column(
           children: [
-            const SizedBox(height: 30,),
-           CustomAppBar(icon: Icons.arrow_back_ios, text: "Mads" , onIconPressed: (){
-             Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => SettingsView()));
-           },),
-
-            SizedBox(height: 25,),
-            // Card 1: Active
-           SizedBox(
-             height: 500,
-             child: ListView.builder(
-               itemCount: 2,
-                 itemBuilder: (contet , index)
-                 {
-                   return InkWell(
-                     onTap: (){
-                       Navigator.pushNamed(context, "/session_activity");
-                     },
-                       child: CardMadsWidget()
-                   );
-                 }
-             ),
-           )
+            context.setMinSize(10).verticalSpace,
+            CustomAppBar(
+              icon: Icons.arrow_back_ios,
+              text: "Mads",
+              onIconPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (_) => const SettingsView()));
+              },
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 2,
+                  itemBuilder: (contet, index) {
+                    return InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, "/session_activity");
+                        },
+                        child: const CardMadsWidget());
+                  }),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
--- lib/Features/Mads/Widgets/card_Mads_Widget.dart
+++ lib/Features/Mads/Widgets/card_Mads_Widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:pro_icon/Core/constants/app_assets.dart';
 import 'package:pro_icon/Core/theme/app_text_styles.dart';
 import 'package:pro_icon/Core/utils/extensions/size_helper.dart';
+import 'package:pro_icon/Core/widgets/custom_svg_visual.dart';
 
 import '../../../Core/theme/app_colors.dart';
 import '../../../Core/utils/responsive_helper/size_config.dart';
@@ -22,101 +23,105 @@ class _CardMadsWidgetState extends State<CardMadsWidget> {
     // TODO:recode this screen to use sizeconfig for responsive
     // TODO:Done Used  this screen to use sizeconfig for responsive
 
-    return   SizeConfig(
-      baseSize: Size(398, 137),
+    return SizeConfig(
+      baseSize: const Size(398, 145),
       width: context.setMinSize(398),
-      height: context.setMinSize(137),
-      child: Builder(
-          builder: (context) {
-            return Container(
-              width: context.sizeConfig.width,
-              height: context.sizeConfig.height,
-              child: Card(
-                color: AppColors.darkGreyColor,
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-
+      height: context.setMinSize(145),
+      child: Builder(builder: (context) {
+        return Container(
+          width: context.sizeConfig.width,
+          height: context.sizeConfig.height,
+          child: Card(
+            color: AppColors.darkGreyColor,
+            elevation: 2,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: context.setMinSize(10),
+                  horizontal: context.setMinSize(20)),
+              child: Column(
+                children: [
+                  Row(
                     children: [
-
-                      Row(
-
-                        children: [
-                          SizedBox(width: 25,),
-                          SvgPicture.asset(Assets.assetsMadsIconSvg),
-                          SizedBox(width: 15,),
-                          Text(
-                              'NO.123456798123',
-                              style: AppTextStyles.fontSize16(context).copyWith(
-                                  color: Colors.white
-                              )
-                          ),
-                          SizedBox(width: 70,),
-                          SvgPicture.asset(Assets.assetsForMadsarrowIcon)
-                        ],
+                      const SizedBox(
+                        width: 25,
+                      ), // use responsive spacing in all widgets
+                      CustomSvgVisual(
+                        assetPath: Assets.assetsMadsIconSvg,
+                        width: context.setMinSize(35),
+                        height: context.setMinSize(35),
                       ),
-                      SizedBox(height: 12),
-                      Row(
-                        children: [
-                          SizedBox(width: 40,),
-                          SvgPicture.asset(Assets.assetsSessionIcon , width: 15, height: 15,),
-                          SizedBox(width: 12,),
-                          Text(
-                              '5 sessions',
-                              style:  AppTextStyles.fontSize14(context).copyWith(
-                                  color: Colors.white
-                              )
-                          ),
-                        ],
+                      const SizedBox(
+                        width: 15,
                       ),
-                      SizedBox(height:  8),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 40 , right: 15),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-
-                            Container(
-                              width: 68,
-                              height: 24,
-                              decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: MaterialButton(
-                                onPressed: () {
-
-                                },
-                                padding: EdgeInsets.zero,
-                                child: Text(
-                                  "Active",
-                                  style: AppTextStyles.fontSize14(context).copyWith(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-
-
-                            SizedBox(width: 10),
-
-                            // الأيقونة SVG
-                            SvgPicture.asset(
-                              Assets.assetsForPowerMads,
-                              height: 24,
-                            ),
-                          ],
-                        ),
+                      Text('NO.123456798123',
+                          style: AppTextStyles.fontSize16(context)
+                              .copyWith(color: Colors.white)),
+                      const SizedBox(
+                        width: 70,
                       ),
+                      SvgPicture.asset(
+                          Assets.assetsForMadsarrowIcon) // use customSvgVisual
                     ],
                   ),
-                ),
+                  const SizedBox(height: 12),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 40,
+                      ),
+                      SvgPicture.asset(
+                        Assets.assetsSessionIcon,
+                        width: 15,
+                        height: 15,
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      Text('5 sessions',
+                          style: AppTextStyles.fontSize14(context)
+                              .copyWith(color: Colors.white)),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: context.setMinSize(80),
+                          height: context.setMinSize(25),
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Active",
+                              style: AppTextStyles.fontSize14(context).copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(width: 10),
+
+                        // الأيقونة SVG
+                        SvgPicture.asset(
+                          Assets.assetsForPowerMads,
+                          height: 24,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            );
-          }
-      ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
